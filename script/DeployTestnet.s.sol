c4dcd19dbe356f88c0fa6c7f84233393c4a1396f
zelinarxy <zelinarxy@proton.me>
2024-02-22 00:07:34 -0500

    change price, split out testnet deploy script

--- script/DeployTestnet.s.sol
+++ script/DeployTestnet.s.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import "../src/RightToTransact.sol";
 
-contract Deploy is Script {
+contract DeployTestnet is Script {
     function run() external {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
 
-        new RightToTransact("Right to Transact", "RTT", 8e15, payable(vm.envAddress("FREN_ADDRESS_MAINNET")));
+        new RightToTransact("Right to Transact", "RTT", 8e15, payable(vm.envAddress("FREN_ADDRESS_TESTNET")));
 
         vm.stopBroadcast();
     }
